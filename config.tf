517e748670e3e8cf0c0a2d5211ee08dad8b17e3a
Мишкин Алексей Игоревич <a.mishkin@tinkoff.ru>
2020-01-13 17:53:09 +0300

    gitignore

--- config.tf
+++ config.tf
@@ -1,7 +1,5 @@
 provider "azurerm" {
     version = "~>1.32.0"
-    client_id = "${var.service_principal_id}"
-    client_secret = "${var.service_principal_key}"
     subscription_id = "${var.subscription_id}"
     tenant_id = "${var.tenant_id}"
 }
