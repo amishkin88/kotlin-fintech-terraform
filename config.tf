e1b4147ac57c12aff954c9b74bd5e96d1ddd774a
Мишкин Алексей Игоревич <a.mishkin@tinkoff.ru>
2020-01-14 10:15:07 +0300

    vars

--- config.tf
+++ config.tf
@@ -1,5 +1,13 @@
 provider "azurerm" {
     version = "~>1.32.0"
-    subscription_id = "${var.subscription_id}"
-    tenant_id = "${var.tenant_id}"
+    subscription_id = var.subscription_id
+    tenant_id = var.tenant_id
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name                = "FintechKotlin-vnet"
+    address_space       = ["10.0.4.0/24"]
+    location            = "West Europe"
+    resource_group_name = "FintechKotlin"
 }
---
+++ config.tfvars
@@ -0,0 +1,2 @@
+tenant_id = ae205af9-f596-4896-939a-ec9690aa9fdf
+subscription_id = f3337cdd-229b-4198-b213-3bd0fb8e82de
