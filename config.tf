f2dc8ce1d98a673a39a31bbe12a3387c5aa14237
Мишкин Алексей Игоревич <a.mishkin@tinkoff.ru>
2020-01-14 10:19:56 +0300

    vars

--- config.tf
+++ config.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
     version = "~>1.32.0"
-    subscription_id = var.subscription_id
-    tenant_id = var.tenant_id
+    subscription_id = "${var.subscription_id}"
+    tenant_id = "${var.tenant_id}"
 }
 
 # Create a virtual network
